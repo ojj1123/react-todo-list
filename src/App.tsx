5ee25b9a9e8cc7560093eacf45080eb912c3214c
Jeongjin <rojay.developer@gmail.com>
2023-11-26 00:49:07 +0900

    feat: 투두리스트 데이터 가져와서 보여주기 * 로딩인디케이터 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,87 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Editable, Loading, TodoListForm } from './components'
+import { TodoListEntity, todoListProvider } from './api'
 
 function App() {
+  const [todoList, setTodoList] = useState<TodoListEntity[]>()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    todoListProvider.getTodoList().then((data) => {
+      setLoading(false)
+      setTodoList(data)
+    })
+  }, [])
+  /**
+   * TODO
+   * - 로딩인디케이터
+   * - 추가, 수정, 삭제 비동기 로직 작성
+   */
+
   return (
     <>
       <h1>오늘 할 일</h1>
       <section>
-        <ul>
-          <li>
-            <input type="checkbox" />
-            <span>나는 오늘 일하러 가야한다</span>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>나는 오늘 일하러 가야한다</span>
-          </li>
-          <li>
-            <input type="text" />
-            <button type="button">취소</button>
-            <button type="button">삭제</button>
-            <button type="button">저장</button>
-          </li>
-          <li>
-            <input type="text" />
-            <button type="button">취소</button>
-            <button type="button">추가</button>
-          </li>
-        </ul>
-        <button type="button">오늘의 할일 추가하기</button>
+        {loading ? (
+          <Loading />
+        ) : (
+          <ul>
+            {todoList
+              ?.filter((todo) => !todo.isArchived)
+              .map((todo) => {
+                return (
+                  <li key={todo.id}>
+                    <Editable.Root>
+                      <Editable.Input type="text" />
+                      <Editable.Preview>
+                        <input type="checkbox" checked={todo.isComplete} />
+                        <Editable.EditTrigger>
+                          <span>{todo.description}</span>
+                        </Editable.EditTrigger>
+                      </Editable.Preview>
+                      <Editable.Control>
+                        {(props) => (
+                          <>
+                            {props.isEditing ? (
+                              <>
+                                <Editable.Cancel>취소</Editable.Cancel>
+                                <Editable.Submit>삭제</Editable.Submit>
+                                <Editable.Submit>저장</Editable.Submit>
+                              </>
+                            ) : null}
+                          </>
+                        )}
+                      </Editable.Control>
+                    </Editable.Root>
+                  </li>
+                )
+              })}
+
+            <TodoListForm />
+          </ul>
+        )}
       </section>
 
       <h1>아카이브</h1>
       <section>
         <ul>
-          <li>
-            <span>나는 오늘 일하러 가야한다</span>
-          </li>
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              {todoList
+                ?.filter((todo) => todo.isArchived)
+                .map((todo) => {
+                  return (
+                    <li key={todo.id}>
+                      <span>{todo.description}</span>
+                    </li>
+                  )
+                })}
+            </>
+          )}
         </ul>
       </section>
     </>
