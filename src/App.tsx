3ef21b01a033175ac1389b41774d89d2e936fcc9
Jeongjin <rojay.developer@gmail.com>
2023-11-25 23:08:05 +0900

    feat(App.tsx): 기본 마크업 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>오늘 할 일</h1>
+      <section>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <span>나는 오늘 일하러 가야한다</span>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <span>나는 오늘 일하러 가야한다</span>
+          </li>
+          <li>
+            <input type="text" />
+            <button type="button">취소</button>
+            <button type="button">삭제</button>
+            <button type="button">저장</button>
+          </li>
+          <li>
+            <input type="text" />
+            <button type="button">취소</button>
+            <button type="button">추가</button>
+          </li>
+        </ul>
+        <button type="button">오늘의 할일 추가하기</button>
+      </section>
+
+      <h1>아카이브</h1>
+      <section>
+        <ul>
+          <li>
+            <span>나는 오늘 일하러 가야한다</span>
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
