8a5ed6f135bf56e3de4cbb16ce1ea90ec28826fa
Jeongjin <rojay.developer@gmail.com>
2023-12-03 16:29:36 +0900

    refactor: migrate useState to useReducer * See https://react.dev/learn/extracting-state-logic-into-a-reducer

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,63 +1,116 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 import { TodoListEntity, todoListProvider } from '../api'
 
+type TData = TodoListEntity[]
 type TError = unknown
-type TData = TodoListEntity[] | null
+
+interface State {
+  data?: TodoListEntity[]
+  error?: TError
+  isLoading: boolean
+  isRefetching: boolean
+}
+
+type Action =
+  | { type: 'fetching' }
+  | { type: 'fetched'; payload: TData }
+  | { type: 'error'; payload: TError }
+  | { type: 'refetching' }
+  | { type: 'refetched'; payload: TData }
+  | { type: 'refetchError'; payload: TError }
+
+const initialState: State = {
+  data: undefined,
+  error: undefined,
+  isLoading: false,
+  isRefetching: false,
+}
+
+const fetchReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'fetching': {
+      return {
+        ...state,
+        isLoading: true,
+      }
+    }
+    case 'fetched': {
+      return {
+        ...state,
+        data: action.payload,
+        error: undefined,
+        isLoading: false,
+      }
+    }
+    case 'error': {
+      return {
+        ...state,
+        data: undefined,
+        error: action.payload,
+        isLoading: false,
+      }
+    }
+    case 'refetching': {
+      return {
+        ...state,
+        isRefetching: true,
+      }
+    }
+    case 'refetched': {
+      return {
+        ...state,
+        data: action.payload,
+        error: undefined,
+        isRefetching: false,
+      }
+    }
+    case 'refetchError': {
+      return {
+        ...state,
+        data: undefined,
+        error: action.payload,
+        isRefetching: false,
+      }
+    }
+  }
+}
 
 export const useFetch = () => {
-  /**
-   * TODO
-   * - useState -> useReducer
-   */
-  const [data, setData] = useState<TData>(null)
-  const [error, setError] = useState<TError>(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const [isRefetch, setIsRefetch] = useState(false)
+  const [state, dispatch] = useReducer(fetchReducer, initialState)
 
   useEffect(() => {
     let active = true
 
-    setIsLoading(true)
+    const fetcher = async () => {
+      dispatch({ type: 'fetching' })
+      try {
+        const data = await todoListProvider.getTodoList()
+        if (active) {
+          dispatch({ type: 'fetched', payload: data })
+        }
+      } catch (error) {
+        if (active) {
+          dispatch({ type: 'error', payload: error })
+        }
+      }
+    }
 
-    todoListProvider
-      .getTodoList()
-      .then((data) => {
-        if (active) {
-          setData(data)
-          setError(null)
-        }
-      })
-      .catch((error) => {
-        if (active) {
-          setData(null)
-          setError(error)
-        }
-      })
-      .finally(() => {
-        if (active) {
-          setIsLoading(false)
-        }
-      })
-
+    fetcher()
     return () => {
       active = false
     }
   }, [])
 
   const refetch = async () => {
-    setIsRefetch(true)
+    dispatch({ type: 'refetching' })
 
     try {
       const data = await todoListProvider.getTodoList()
-      setData(data)
-      setError(null)
+      dispatch({ type: 'refetched', payload: data })
     } catch (error) {
-      setData(null)
-      setError(error)
-    } finally {
-      setIsRefetch(false)
+      dispatch({ type: 'refetchError', payload: error })
     }
   }
 
-  return { data, error, isLoading, isRefetch, refetch } as const
+  return { ...state, refetch } as const
 }
